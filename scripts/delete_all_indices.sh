1514688777ad8fa239e2f159085712969eaa9a09
Matthew C Jones <mjones@isaudits.com>
2018-06-15 01:25:15 -0400

    add sudo check

--- scripts/delete_all_indices.sh
+++ scripts/delete_all_indices.sh
@@ -2,6 +2,12 @@
 # This script will delete all logstash indices from Elasticsearch database
 # Use with caution!!!
 
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 so-elastic-stop
-sudo docker exec so-curator curator_cli --config /etc/curator/config/curator.yml delete_indices --filter_list '[{"filtertype":"pattern","kind":"prefix","value":"logstash"}]'
+docker exec so-curator curator_cli --config /etc/curator/config/curator.yml delete_indices --filter_list '[{"filtertype":"pattern","kind":"prefix","value":"logstash"}]'
 so-elastic-start
