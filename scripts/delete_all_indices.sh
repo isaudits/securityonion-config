6dbf940df35597ab7275858a03e3b0569651b72d
Matthew C Jones <mjones@isaudits.com>
2018-06-15 01:20:00 -0400

    add service stop and start to log purge script

--- scripts/delete_all_indices.sh
+++ scripts/delete_all_indices.sh
@@ -2,4 +2,6 @@
 # This script will delete all logstash indices from Elasticsearch database
 # Use with caution!!!
 
+so-elastic-stop
 sudo docker exec so-curator curator_cli --config /etc/curator/config/curator.yml delete_indices --filter_list '[{"filtertype":"pattern","kind":"prefix","value":"logstash"}]'
+so-elastic-start
